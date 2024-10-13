add3d54c3826812190c0897b52b5d01a0c0ed17d
Mateusz-Szewczyk <mateusz.szewczyk000@gmail.com>
2024-10-13 19:51:23 +0200

    main for testing

--- rag/main.py
+++ rag/main.py
@@ -1,29 +1,47 @@
-from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
-from src.metadata_extraction import extract_metadata_using_llm
-from src.vector_store import create_vector_store
-from src.graph_store import create_graph_entries, create_entity_relationships
 from src.search_engine import search_and_rerank
-from src.file_processor.pdf_processor import PDFProcessor
+from file_processor.pdf_processor import PDFProcessor
 from src.chunking import create_chunks
 
-def main():
-    # Sample data setup (replace with actual data)
-    # pdfprocessor = PDFProcessor()
-    # text = pdfprocessor.process_pdf('./data/pulapki-myslenia.pdf', start_page=89, end_page=90)
+def display_results(results):
+    print("\nWyniki wyszukiwania i rerankingu:")
+    for result in results:
+        source = result.get('source', 'unknown')
+        content = result.get('content', '')
+        similarity_score = result.get('similarity_score', 0)
+        normalized_score = result.get('normalized_score', 0)
+        metadata = result.get('metadata', {})
 
-    # if text is None:
-    #     print("Error: Failed to process PDF.")
-    #     return
+        print(f"Źródło: {source}")
+        print(f"Treść:\n{content}\n")
+
+        if metadata:
+            if 'entities' in metadata and metadata['entities']:
+                print(f"Encje: {metadata['entities']}")
+            if 'relations' in metadata and metadata['relations']:
+                print(f"Relacje: {metadata['relations']}")
+        print(f"Wynik podobieństwa: {similarity_score:.4f}")
+        print(f"Wynik znormalizowany: {normalized_score:.4f}")
+        print("-" * 40)
+
+
+def main():
+    # # Sample data setup (replace with actual data)
+    pdfprocessor = PDFProcessor()
+    text = pdfprocessor.process_pdf('./tests/test_files/test.pdf', start_page=7, end_page=90)
+    #
+    if text is None:
+        print("Error: Failed to process PDF.")
+        return
 
     user_id = 'user123'
-    # file_description = 'To jest książka o psychologii'
-    # category = 'psychologia'
-    #
+    file_description = 'To jest książka o psychologii'
+    category = 'psychologia'
+
     # # Chunking the text
-    # chunks = create_chunks(text)
-    # embeddings = []
-    # extracted_metadatas = []
+    chunks = create_chunks(text)
+    embeddings = []
+    extracted_metadatas = []
 
     # Initialize the embedding model
     embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -51,27 +69,13 @@ def main():
     #     print("Metadata: ", metadata)
 
     # Now, use the search engine to query the data
-    query = "Czym jest efekt halo?"
+    query = "Wytłumacz mi czym jest System 1 i System 2"
     results = search_and_rerank(query, embedding_model, user_id, n_results=5)
 
     # Print the results
-    print("\nWyniki wyszukiwania i rerankingu:")
-    for result in results:
-        source = result.get('source', 'unknown')
-        content = result.get('content', '')
-        similarity_score = result.get('similarity_score', 0)
-        normalized_score = result.get('normalized_score', 0)
-        metadata = result.get('metadata', {})
+    display_results(results)
 
-        print(f"Źródło: {source}")
-        print(f"Treść:\n{content}\n")
-        if 'entities' in metadata:
-            print(f"Encje: {metadata['entities']}")
-        if 'relations' in metadata:
-            print(f"Relacje: {metadata['relations']}")
-        print(f"Wynik podobieństwa: {similarity_score:.4f}")
-        print(f"Wynik znormalizowany: {normalized_score:.4f}")
-        print("-" * 40)
+
 
 
 if __name__ == '__main__':
