23faafdf4deee420371a1aee5f34fea282b4fe15
Mateusz-Szewczyk <mateusz.szewczyk000@gmail.com>
2025-01-09 23:05:39 +0100

    fix requirements

--- backend/server/config.py
+++ backend/server/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    DATABASE_URL: str = f'postgresql+psycopg://{os.getenv('USER_DB')}:{os.getenv('PASSWORD_DB')}@{os.getenv('SERVER')}/{os.getenv('POSTGRES_DB')}'
+    DATABASE_URL: str = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODYFICATIONS: bool = False
     SECRET_KEY: str | None = os.getenv('SECRET_KEY')
 
